36e8e2ab4815fc5d9fcd87971b4297e1a50aa608
Бурдейник Александр Витальевич <burdeinick@yandex.ru>
2020-05-19 12:31:51 +0300

    change

--- Coursera/ООП и паттерны проектирования в Python/Week_4_Pattern_Chain_of_Responsibility.py
+++ Coursera/ООП и паттерны проектирования в Python/Week_4_Pattern_Chain_of_Responsibility.py
@@ -130,24 +130,24 @@ class StrHandler(NullHandler):
 
                 return super().handle(char, event)
 
+if __name__ == '__main__':
+    obj = SomeObject()
+    obj.integer_field = 42
+    obj.float_field = 3.14
+    obj.string_field = "some text"
 
-obj = SomeObject()
-obj.integer_field = 42
-obj.float_field = 3.14
-obj.string_field = "some text"
+    chain = IntHandler(FloatHandler(StrHandler(NullHandler)))
+    print(chain.handle(obj, EventGet(int)))
 
-chain = IntHandler(FloatHandler(StrHandler(NullHandler)))
-print(chain.handle(obj, EventGet(int)))
+    print(chain.handle(obj, EventGet(float)))
 
-print(chain.handle(obj, EventGet(float)))
+    print(chain.handle(obj, EventGet(str)))
 
-print(chain.handle(obj, EventGet(str)))
+    chain.handle(obj, EventSet(100))
+    print(chain.handle(obj, EventGet(int)))
 
-chain.handle(obj, EventSet(100))
-print(chain.handle(obj, EventGet(int)))
+    chain.handle(obj, EventSet(0.5))
+    print(chain.handle(obj, EventGet(float)))
 
-chain.handle(obj, EventSet(0.5))
-print(chain.handle(obj, EventGet(float)))
-
-chain.handle(obj, EventSet('new text'))
-print(chain.handle(obj, EventGet(str)))
+    chain.handle(obj, EventSet('new text'))
+    print(chain.handle(obj, EventGet(str)))
