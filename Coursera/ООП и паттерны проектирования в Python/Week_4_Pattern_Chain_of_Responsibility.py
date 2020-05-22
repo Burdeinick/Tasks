21e89acda510c29e63cbc3a8cf86b8af8c62f085
Бурдейник Александр Витальевич <burdeinick@yandex.ru>
2020-05-22 13:36:49 +0300

    test

--- Coursera/ООП и паттерны проектирования в Python/Week_4_Pattern_Chain_of_Responsibility.py
+++ Coursera/ООП и паттерны проектирования в Python/Week_4_Pattern_Chain_of_Responsibility.py
@@ -4,7 +4,7 @@ class SomeObject:
     def __init__(self):
         self.integer_field = 0
         self.float_field = 0.0
-        self.string_field = ""  #
+        self.string_field = ""
 
 
 class EventGet:
