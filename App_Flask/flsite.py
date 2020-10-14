e32db96512911cd7004fdd70b6e5fd05d3474b25
Burdeinick <burdeinick@yandex.ru>
2020-10-14 13:50:51 +0300

    add login, profile

--- App_Flask/flsite.py
+++ App_Flask/flsite.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '0525'
 
 menu = [
         {"name": "Установка", "url": "install-flask"},
@@ -21,17 +22,65 @@ def about():
     return render_template("about.html", title="О сайте", menu=menu)
 
 
-@app.route("/profile/<int:username>")
+@app.route("/profile/<username>")
 def profile(username):
-    return f"ПОльзователь: {username}"
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+    return f"Пользователь: {username}"
     
 
 @app.route("/contact", methods = ["POST", "GET"])
 def contact():
-    if request.method == "POST":
-        print(request.form['username'])
+    if request.method == 'POST':
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
 
     return render_template("contact.html", title="Обратная связь", menu=menu)
+
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    elif request.method == 'POST' and request.form['username'] == "selfedu" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+
+    return render_template('login.html', title="Авторизация", menu=menu)
+
+
+# @app.route("/profile")
+# def profile(username):
+#     # if 'userLogged' not in session or session['userLogged'] != username:
+#     # abort(401)
+
+#     return f"Профиль пользователя: "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.errorhandler(404)
+def pageNotFount(error):
+    return render_template('page404.html', title="Страница не найдена", menu=menu), 404
 
 
 if __name__ == "__main__":
